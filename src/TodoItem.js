e9d0f801993fb865cb75df45043a3348795bf773
Dell <nafishasnain0@gmail.com>
2020-05-14 12:25:41 -0400

    Add TodoItem component

---
+++ src/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TodoItem.css";
+
+function TodoItem(){
+    return(
+        <div>
+            <input type="checkbox"></input>
+            <p>Placeholder text here</p>
+        </div>
+    )
+}
+
+export default TodoItem;
