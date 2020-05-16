a0e896aa54feec91119fc57808fcad72fd00ce3f
Dell <nafishasnain0@gmail.com>
2020-05-16 16:13:51 -0400

    Fix a  typo

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,7 @@ import "./TodoItem.css";
 
 function TodoItem(props){
     return(
-        <div className="todo-item" style={{display: props.completed %% "none"}}>
+        <div className="todo-item" style={{display: props.completed && "none"}}>
             <input type="checkbox"></input>
             <p>{props.text}</p>
         </div>
