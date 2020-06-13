9c146b71b70f76629cb1035f7f614486591a1b93
Dell <nafishasnain0@gmail.com>
2020-06-13 15:37:50 -0400

    Add state and toggle method for theme

--- src/themeContext.js
+++ src/themeContext.js
@@ -2,10 +2,23 @@ import React, {Component} from "react"
 const {Provider, Consumer} = React.createContext()
 
 class ThemeContextProvider extends Component {
+    state = {
+        theme: "light"
+    }
+
+    toggle = () => {
+        this.setState(prevState => {
+            return {
+                theme: prevState.theme === "light" ? "dark" : "light"
+            }
+        })
+    }
+
     render() {
         return (
-            <Provider value={"dark"}>
+            <Provider value={this.state.theme}>
                 {this.props.children}
+                <button onClick={this.toggle}>Changer</button>
             </Provider>
         )
     }
