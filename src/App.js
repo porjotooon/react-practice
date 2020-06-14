40e5b2916fa90c4095528014bc8a7a4c75331a00
Dell <nafishasnain0@gmail.com>
2020-06-14 17:04:35 -0400

    Corrected method to stop state mutation

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import React, {useState} from "react"
 
 
-// class App extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             count: 0
-//         }
-//     }
-    
-//     render() {
-//         return (
-//             <div>
-//                 <h1>{this.state.count}</h1>
-//                 <button>Change!</button>
-//             </div>
-//         )
-//     }
-// }
-
 const App = () => {
     const [ count, setCount ] = useState(0)
+
+    function increment(){
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function deccrement(){
+        setCount(prevCount => prevCount - 1)
+    }
+
     return (
             <div>
                 <h1>{ count }</h1>
-                <button onClick={() =>setCount(count+1)}>Increase</button>
+                <button onClick={increment}>Increase</button>
                 <br></br>
-                <button onClick={() =>setCount(count-1)}>Decrease</button>
+                <button onClick={deccrement}>Decrease</button>
             </div>        
     )
 }
