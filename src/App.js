267ee337b463284442039361abe8d3f9fe7114e1
Dell <nafishasnain0@gmail.com>
2020-06-14 15:06:24 -0400

    Hooks practice session

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react"
 
-import Header from "./Header"
-import Button from "./Button"
-
 function App() {
     return (
         <div>
-            <Header />
-            <Button />
         </div>
     )
 }
