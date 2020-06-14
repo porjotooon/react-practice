df9ce3036e54587c5b09640b488d602ff3fd131f
Dell <nafishasnain0@gmail.com>
2020-06-14 16:59:35 -0400

    Created counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
-import React from "react"
+import React, {useState} from "react"
 
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            count: 0
-        }
-    }
+// class App extends React.Component {
+//     constructor() {
+//         super()
+//         this.state = {
+//             count: 0
+//         }
+//     }
     
-    render() {
-        return (
+//     render() {
+//         return (
+//             <div>
+//                 <h1>{this.state.count}</h1>
+//                 <button>Change!</button>
+//             </div>
+//         )
+//     }
+// }
+
+const App = () => {
+    const [ count, setCount ] = useState(0)
+    return (
             <div>
-                <h1>{this.state.count}</h1>
-                <button>Change!</button>
-            </div>
-        )
-    }
+                <h1>{ count }</h1>
+                <button onClick={() =>setCount(count+1)}>Increase</button>
+                <br></br>
+                <button onClick={() =>setCount(count-1)}>Decrease</button>
+            </div>        
+    )
 }
 
 export default App
