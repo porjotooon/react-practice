9dafeee92035a85ff856a3e7989787b70e6bda99
Dell <nafishasnain0@gmail.com>
2020-06-20 17:24:14 -0400

    Convert class component to funcational component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react"
+import React, {useState} from "react"
 /**
  * Pop Quiz!
  * 
@@ -6,23 +6,36 @@ import React, {Component} from "react"
  */
 
 
-class App extends Component {   
-    state = {
-        count: 0
-    }
+// class App extends Component {   
+//     state = {
+//         count: 0
+//     }
     
-    increment = () => {
-        this.setState(prevState => ({count: prevState.count + 1}))
-    }
+//     increment = () => {
+//         this.setState(prevState => ({count: prevState.count + 1}))
+//     }
     
-    render() {
-        return (
-            <div>
-                <h1>The count is {this.state.count}</h1>
-                <button onClick={this.increment}>Add 1</button>
-            </div>
-        )
+//     render() {
+//         return (
+//             <div>
+//                 <h1>The count is {this.state.count}</h1>
+//                 <button onClick={this.increment}>Add 1</button>
+//             </div>
+//         )
+//     }
+// }
+
+function App(){
+    const [ count, setCount ] = useState(0)
+    const increment = () => {
+        setCount(prevCount => 1 + prevCount)
     }
+    return (
+        <div>
+            <h1>The count is {count}</h1>
+            <button onClick={increment}>Add 1</button>
+        </div>
+    )
 }
 
 export default App
