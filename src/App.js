de766885e52d8c33966e09617e9d8b170b0272f3
Dell <nafishasnain0@gmail.com>
2020-06-20 17:32:06 -0400

    Custom Hooks refactor session

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import React, {useState} from "react"
-/**
- * Pop Quiz!
- * 
- * Refactor the class component below to use hooks instead
- */
+import React from "react"
+import Menu from "./Menu"
+import Favorite from "./Favorite"
 
-
-// class App extends Component {   
-//     state = {
-//         count: 0
-//     }
-    
-//     increment = () => {
-//         this.setState(prevState => ({count: prevState.count + 1}))
-//     }
-    
-//     render() {
-//         return (
-//             <div>
-//                 <h1>The count is {this.state.count}</h1>
-//                 <button onClick={this.increment}>Add 1</button>
-//             </div>
-//         )
-//     }
-// }
-
-function App(){
-    const [ count, setCount ] = useState(0)
-    const increment = () => {
-        setCount(prevCount => 1 + prevCount)
-    }
+function App() {
     return (
         <div>
-            <h1>The count is {count}</h1>
-            <button onClick={increment}>Add 1</button>
+            <Menu />
+            <hr />
+            <Favorite />
         </div>
     )
 }
