cd13dcf5c05761b985d7c6b011619be6040b067d
Dell <nafishasnain0@gmail.com>
2020-07-05 11:02:55 -0400

    Add navbar with only page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react"
-
+import Header from './components/Header'
+import {Switch,Route} from 'react-router-dom'
+import Home from './pages/Home'
 function App() {
     return (
         <div>
+            <Header/>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+            </Switch>
         </div>
     )
 }
