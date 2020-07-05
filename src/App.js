86f41af620a257a71fb8ee53ade5c20cb77cd33e
Dell <nafishasnain0@gmail.com>
2020-07-05 10:43:30 -0400

    React Router session

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import React from "react"
-import Menu from "./Menu"
-import Favorite from "./Favorite"
 
 function App() {
     return (
         <div>
-            <Menu />
-            <hr />
-            <Favorite />
         </div>
     )
 }
